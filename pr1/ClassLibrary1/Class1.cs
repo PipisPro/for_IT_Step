5c6c963e476f0e5affb418ee7c5278ca2d24be08
PipisPro <cyrax_96@mail.ru>
2020-01-08 15:47:36 +0500

    Pr_1_07_01_2020
    Project 1

---
+++ pr1/ClassLibrary1/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class User
+    {
+        private string FName;
+        private string LName;
+        private string Email;
+        private string Username;
+        private string Password;
+
+        public User(string f, string l, string e, string u, string p)
+        {
+            FName = f;
+            LName = l;
+            Email = e;
+            Username = u;
+            Password = p;
+        }
+        public void set_FName(string s)
+        {
+            FName = s;
+        }
+        public void set_LName(string s)
+        {
+            LName = s;
+        }
+        public void set_Email(string s)
+        {
+            Email = s;
+        }
+        public void set_Username(string s)
+        {
+            Username = s;
+        }
+        public void set_Password(string s)
+        {
+            Password = s;
+        }
+
+        public void set_Master(string f, string l, string e, string u, string p)
+        {
+            set_FName(f);
+            set_LName(l);
+            set_Email(e);
+            set_Username(u);
+            set_Password(p);
+        }
+    }
+}
---
+++ pr1/pr1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace pr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 15;
+            int b = 35;
+            string s = "Your sallary is: ";
+
+            User wasser = new User("First", "Last", "Email", "Username", "Password");
+
+        //  wasser.set_Master("First", "Last", "Email", "Username", "Password");
+            
+            Console.WriteLine(s += a + b);  // (s + a + b) - неявная типизация
+            Console.ReadKey();
+        }
+    }
+}
